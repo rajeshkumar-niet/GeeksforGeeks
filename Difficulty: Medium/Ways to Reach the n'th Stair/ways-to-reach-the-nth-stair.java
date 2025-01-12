2abc8c50172b665cf13b68929e8a4d3ba25db225
RAJESH KUMAR <103594210+rajeshkumar-niet@users.noreply.github.com>
2025-01-12 23:13:43 +0530

    Update ways-to-reach-the-nth-stair.java

--- Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
+++ Difficulty: Medium/Ways to Reach the n'th Stair/ways-to-reach-the-nth-stair.java
@@ -32,10 +32,40 @@ class GFG {
 
 class Solution {
     // Function to count number of ways to reach the nth stair.
+    // DP bottom up time 0.22
     int countWays(int n) {
         // your code here
         if(n <= 2) return n;
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
         
-        return countWays(n-1) + countWays(n-2);
+        return dp[n];
     }
+    
+    // static int count(int n, int[] dp){
+    //     if(n <= 2) return n;
+    //     if(dp[n] != 0)
+    //         return dp[n];
+        
+    //     dp[n] = count(n-1, dp)+ count(n-2, dp);
+    //     return dp[n];
+    // }
+    // // DP top-down time 0.24
+    // int countWays(int n) {
+    //     // your code here
+    //     if(n <= 2) return n;
+    //     int[] dp = new int[n+1];
+    //     return count(n, dp);
+    // }
+    
+    // Recursion time 0.26
+    // int countWays(int n) {
+    //     // your code here
+    //     if(n <= 2) return n;
+    //     return countWays(n-1) + countWays(n-2);
+    // }
 }
