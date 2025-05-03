9645c0e9be1d3916c17db0ee3b4ad6a17b35ab97
RAJESH KUMAR <103594210+rajeshkumar-niet@users.noreply.github.com>
2025-05-03 11:44:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCases = Integer.parseInt(br.readLine());
+
+        while (testCases-- > 0) {
+            int capacity = Integer.parseInt(br.readLine());
+            String[] valInput = br.readLine().split(" ");
+            String[] wtInput = br.readLine().split(" ");
+
+            int[] val = new int[valInput.length];
+            int[] wt = new int[wtInput.length];
+
+            for (int i = 0; i < valInput.length; i++) {
+                val[i] = Integer.parseInt(valInput[i]);
+            }
+
+            for (int i = 0; i < wtInput.length; i++) {
+                wt[i] = Integer.parseInt(wtInput[i]);
+            }
+
+            System.out.println(Solution.knapsack(capacity, val, wt));
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int knapSack(int W, int wt[], int val[], int n) 
+    { 
+         // your code here 
+        int[][] dp = new int[n + 1][W + 1];
+        
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= W; w++) {
+                if (i == 0 || w == 0) {
+                    dp[i][w] = 0;
+                } else if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+        
+        return dp[n][W];
+    }
+    static int knapsack(int W, int val[], int wt[]) {
+        // code here
+        return knapSack(W, wt, val , val.length);
+    }
+}
